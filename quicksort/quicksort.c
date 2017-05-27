edfb3ebcf02fee293e81de6ed531c03d2da19491
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-27 16:05:12 -0300

    Add functional quicksort.c

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,14 +1,13 @@
-#define ELF_DIE(X) fprintf(stdout, "%s:%s:%d - %s", __FILE__, __func__, __LINE__, X), exit(EXIT_FAILURE)
-#define ELF_MAX(X,Y) ((X)>(Y)?X:Y)
-#define ELF_MIN(X,Y) ((X)<(Y)?X:Y)
-#define ELF_ABS(X) ((X)<0?-(X):X)
-#define ELF_MEDIAN(X, Y, Z) (X<=Y?(X>=Z?X:(Z<=Y?Z:Y)):(Y>=Z?Y:(X<=Z?X:Z)))
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEDIAN(X, Y, Z) (X<=Y?(X>=Z?X:(Z<=Y?Z:Y)):(Y>=Z?Y:(X<=Z?X:Z)))
 
 static
 void partition_ascend(int *vec, int left, int right, int *left_of_mid, int *right_of_mid){
 	int pivot, aux;
 
-	pivot = ELF_MEDIAN(vec[left], vec[right], vec[(left+right)/2]);
+	pivot = MEDIAN(vec[left], vec[right], vec[(left+right)/2]);
 	
 	while(left <= right){
 		while(vec[left] < pivot) left++;
@@ -31,19 +30,27 @@ void partition_ascend(int *vec, int left, int right, int *left_of_mid, int *righ
 
 // Recursive part of quicksort.
 static
-void quicksort_op_ascend(int *vec, int left, int right){
+void quicksort(int *vec, int left, int right){
 	int i, j;
 
 	if(right <= left) return;
 	partition_ascend(vec, left, right, &i, &j);
-	quicksort_op_ascend(vec, left, i);
-	quicksort_op_ascend(vec, j, right);
+	quicksort(vec, left, i);
+	quicksort(vec, j, right);
 }
 
-// Documented in header file.
-void elfIntVector_qsort_ascend(ElfIntVector *elf){
-	if(!elf) ELF_DIE("NULL pointer received!");
-	if(elf->size <= 1) return;
+int main(int argc, char *argv[]){
+	int vector[1000];
+	int i, size = 0;
 
-	quicksort_op_ascend(elf->vector, 0, elf->size - 1, NULL);
+	while(scanf("%d", &vector[size]) == 1)
+		size++;
+
+	quicksort(vector, 0, size-1);
+
+	for(i = 0; i < size; i++)
+		printf("%d ", vector[i]);
+	printf("\n");
+
+	return 0;
 }
